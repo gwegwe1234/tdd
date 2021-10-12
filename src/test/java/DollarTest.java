675fbf5f6dc685028f9a9b60c0e682300117259e
gwegwe1234 <gwangtae5860@daum.net>
2021-10-13 08:58:32 +0900

    4장 추가 +

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -10,19 +10,6 @@ public class DollarTest {
   @Test
   public void testMultiplication() {
     Dollar five = new Dollar(5);
-//    five.times(2);
-//    assertEquals(10, five.amount);
-//    five.times(3);
-//    assertEquals(15, five.amount);
-    Dollar product = five.times(2);
-    assertEquals(10, product.amount);
-    product = five.times(3);
-    assertEquals(15, product.amount);
-  }
-
-  @Test
-  public void testMultiplication4chapter() {
-    Dollar five = new Dollar(5);
     assertEquals(new Dollar(10), five.times(2));
     assertEquals(new Dollar(15), five.times(3));
   }
