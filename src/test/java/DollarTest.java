225eb0ecedadc50718b637cae5bdfe7171d0e4b6
gwegwe1234 <gwangtae5860@daum.net>
2021-10-13 08:56:27 +0900

    4장 추가

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -21,6 +21,13 @@ public class DollarTest {
   }
 
   @Test
+  public void testMultiplication4chapter() {
+    Dollar five = new Dollar(5);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
+  }
+
+  @Test
   public void testEquality() {
     assertTrue(new Dollar(5).equals(new Dollar(5)));
     assertFalse(new Dollar(5).equals(new Dollar(6)));
@@ -29,7 +36,7 @@ public class DollarTest {
 
 class Dollar {
 
-  int amount;
+  private int amount;
 
   public Dollar(int amount) {
     this.amount = amount;
